66c88dd11cb573d39604e860904348472d27911b
akhilmhdh <akhilmhdh@gmail.com>
2021-06-02 16:07:43 +0530

    feat: added sort object tracking

--- pixmo/main.py
+++ pixmo/main.py
@@ -5,6 +5,8 @@ import os
 from os import path
 import argparse
 
+from pixmo.sort import Sort
+
 BASE_DIR = os.path.dirname(__file__)
 
 
@@ -34,24 +36,49 @@ def img2tensor(img):
 
 
 def start_pixmo():
-    image = cv2.imread(path.join(BASE_DIR, "test/dhoni.jpeg"))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    object_tracker = Sort()
     face_cascade = cv2.CascadeClassifier(
         path.join(BASE_DIR, "models/haarcascade_frontalface_default.xml")
     )
-    faces = face_cascade.detectMultiScale(image, 1.3, 5)
-    for i, (x, y, w, h) in enumerate(faces):
-        wpad = int(w * pad_scale)
-        hpad = int(h * pad_scale)
-        face_img = image[y - hpad : y + h + hpad, x - wpad : x + w + wpad]
-        face_img = cv2.resize(face_img, (48, 48))
-        face_img_tensor = img2tensor(face_img)[None][None]
-        print(face_img_tensor.shape)
-        ort_inputs = {ort_session.get_inputs()[0].name: face_img_tensor}
-        ort_outs = ort_session.run(None, ort_inputs)
-        output = ort_outs[0]
-        pred = np.argmax(output[0])
-        print(classes[pred])
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(image, 1.3, 5)
+        faces = (
+            np.array([[*face, 1] for face in faces]) if len(faces) else np.empty((0, 5))
+        )
+        track_bbs_ids = object_tracker.update(faces)
+        for i, (x, y, w, h, object_id) in enumerate(faces):
+            wpad = int(w * pad_scale)
+            hpad = int(h * pad_scale)
+            face_img = image[y - hpad : y + h + hpad, x - wpad : x + w + wpad]
+            face_img = cv2.resize(face_img, (48, 48))
+            face_img_tensor = img2tensor(face_img)[None][None]
+            ort_inputs = {ort_session.get_inputs()[0].name: face_img_tensor}
+            ort_outs = ort_session.run(None, ort_inputs)
+            output = ort_outs[0]
+            pred = np.argmax(output[0])
+            cv2.rectangle(frame, (x, y), (x + w, x + h), (0, 255, 0), 2)
+            cv2.putText(
+                frame,
+                f"{classes[pred]} - person:{object_id}",
+                (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                (36, 255, 12),
+                2,
+            )
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
 
 
 def parse_args():
