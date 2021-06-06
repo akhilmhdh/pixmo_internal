dfb6d043cc93c5f22a7f4607c74538e5b93d2d7c
akhilmhdh <akhilmhdh@gmail.com>
2021-06-06 13:26:09 +0530

    feat: added multiprocessing queue

--- pixmo/main.py
+++ pixmo/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from os import path
 import argparse
+from multiprocessing import Process, Queue
 
 from pixmo.sort import Sort
 from pixmo.detectors.yolo import (
@@ -89,7 +90,20 @@ def img2tensor(img):
 # cap.release()
 
 
+def event_loop(queue: Queue):
+    while True:
+        msg = queue.get()
+        if msg["state"] == "end":
+            break
+        print(f"{msg['state']}-{msg['payload']}")
+
+
 def start_pixmo():
+    event_queue = Queue(maxsize=50)
+    event_process = Process(target=event_loop, args=((event_queue),))
+    event_process.daemon = True
+    event_process.start()
+
     object_tracker = Sort(max_age=10, min_hits=3, iou_threshold=0.3)
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
@@ -106,6 +120,8 @@ def start_pixmo():
         key = cv2.waitKey(1)
         if key == 27:
             break
+    event_queue.put({"state": "end"})
+    event_process.join()
     cap.release()
 
 
